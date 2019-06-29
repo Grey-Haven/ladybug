bda30f6f7353bf92d96e7af04d465c84039a8791
stewhi <stewhi@microsoft.com>
2019-06-29 11:24:06 -0700

    Trying to bump the js.

--- js/revelation.js
+++ js/revelation.js
@@ -1,5 +1,5 @@
 function revelation() {
     if (document.getElementById("message").value === "I saw the light") {
-        window.open('./mp3/The Gospel of Sand.mp3')
+        window.open('./mp3/The Gospel of Sand.m p3')
     }                
 }
